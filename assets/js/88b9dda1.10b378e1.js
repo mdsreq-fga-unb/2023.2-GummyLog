da913741b5bf3026519b91465cde0e230ed06bed
Dridr1 <a.driano@outlook.com>
2023-09-21 13:44:32 -0300

    professor eu confesso que isso é uma gambiarra cabulosa. PERDÃO!

---
+++ assets/js/88b9dda1.10b378e1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgummy_store_docs=self.webpackChunkgummy_store_docs||[]).push([[793],{5745:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
