af4b133902b9a092cb376475f642f5738f74ed0b
Dridr1 <adriano.costa@dafiti.com.br>
2023-11-14 13:11:50 -0300

    feat: adicionada rota de health check

--- src/router/router.js
+++ src/router/router.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import marcasRouter from "./marcasRouter.js";
 import unidadeDeArmazenamentoRouter from "./unidadeDeArmazenamentoRouter.js";
 import SKURouter  from "./SKURouter.js";
+import { healthCheck } from "../controllers/healthController.js";
 
 const router = Router();
 
 router.use(marcasRouter);
 router.use(unidadeDeArmazenamentoRouter);
 router.use(SKURouter);
+router.use(healthCheck);
 
 export default router; 
