5e8a0d53f2b65980cc1e130868906132752e7f27
Sebastian Zuzunaga <seb55381@hotmail.com>
2023-12-09 17:44:31 -0500

    Feat: adicioadas interface de registro e login de usuario

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TaskTableSKU from "./components/tabelaSKU/TaskTableSKU";
 import TaskTableProdutos from "./components/tabelaProdutos/TaskTableProdutos"
+import UserRegister from "./components/userRegister/UserRegisterForm"
+import UserLogin from "./components/userLogin/UserLoginForm"
 import React from 'react'
 
 const Router = () => {
@@ -9,6 +11,8 @@ const Router = () => {
         <Routes>
             <Route path="/skus" element={<TaskTableSKU/>}/>
             <Route path="/produtos" element={<TaskTableProdutos/>}/>
+            <Route path="/registeruser" element={<UserRegister/>}/>
+            <Route path="/loginuser" element={<UserLogin/>}/>
         </Routes>
     </BrowserRouter>
   )
