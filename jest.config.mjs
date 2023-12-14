717d2db8eb726994abd63ac5413e5e41d7aba341
junioramaral22 <junior.amaral.1818@gmail.com>
2023-12-14 02:01:08 -0300

    test: os testes agora rodam corretamente com import e export

---
+++ jest.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  transform: {},
+};
+
+export default config;
